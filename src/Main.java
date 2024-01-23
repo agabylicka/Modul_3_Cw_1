351e1e137bede9e348894d17191c32b3a273fe44
Agnieszka <aga_bylicka@poczta.onet.pl>
2024-01-23 18:05:29 +0100

    Changed "for loop".

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import org.trzcinska.Suma;
 public class Main {
     public static void main(String[] args) {
         int[] array = new int[10];
-        for( int i= 0; i< 10; i++) {
+        for( int i= 0; i< array.length; i++) {
             array[i] = i;
         }
         Suma dowolna = new Suma();
